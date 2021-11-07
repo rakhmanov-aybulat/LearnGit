a80efbfe5f450d7e4064ae7a639b2b9f1eafe356
DedPerd <dedperd1@gmail.com>
2021-11-07 14:49:48 +0300

    made other changes

--- test.js
+++ test.js
@@ -1,11 +1,25 @@
 'use strict'
 
-export default class Game {
+class Game {
     constructor(columns, rows) {
         this.columns = columns;
 	this.rows = rows;
     }
     movePieceDown() {
 	console.log('движение вниз')
+    }
+}
+class Controller {
+    constructor(game) {
+        this.game = game;
+        this.speed = 800;
+        this.timerId = null;
+    }
+    startTimer() {
+        let tick;
+        this.timerId = setTimeout(tick = () => {
+            this.game.movePieceDown();
+            this.timerId = setInterval(tick, this.speed);
+        }, this.speed);
     }
 }
